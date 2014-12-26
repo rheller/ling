c7ab7b3bb1b84068aabe28ad53c93ddbaed353f0
Rick Heller <rick.heller@yahoo.com>
2014-12-26 13:29:31 -0500

    redirect to home from the root if the user is signed in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def front
-#    if current_user
-#      redirect_to home_path
-#    end
+    if current_user
+      redirect_to home_path
+    end
   end
 
 end
