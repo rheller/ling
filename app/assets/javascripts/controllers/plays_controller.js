13bd16ca854e3ce1d81e26520751bab582683f93
Rick Heller <rick.heller@yahoo.com>
2015-01-09 16:03:01 -0500

    create new play and successfully pass to server

--- app/assets/javascripts/controllers/plays_controller.js
+++ app/assets/javascripts/controllers/plays_controller.js
@@ -1,11 +1,12 @@
 Ling.PlaysController = Ember.ArrayController.extend({
   actions: {
-    updatePlay: function() {
-      var play = this.get('content');
-      play.set('title', this.get('title'));
-      play.set('body', this.get('body'));
-//       play_id: hidden_val,
-  //      original_spelling: 'hobo',
+    updatePlay: function(chosen, play_id) {
+
+     var play = this.store.createRecord('play', {
+        play_id: play_id,
+        success_rate: chosen
+      });
+
       var controller = this;
       play.save().then(function() {
         console.log('play saved!');
