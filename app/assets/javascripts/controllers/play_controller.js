ca670c722bcb6c0f4a19f88ccb01eb9b31fc388b
Rick Heller <rick.heller@yahoo.com>
2015-01-10 11:43:40 -0500

    add div for ember messages

--- app/assets/javascripts/controllers/play_controller.js
+++ app/assets/javascripts/controllers/play_controller.js
@@ -10,6 +10,7 @@ Ling.PlayController = Ember.ObjectController.extend({
       var controller = this;
       play.save().then(function() {
         console.log('play saved!');
+      $('#ember-messages').html('play saved!!!!')
         controller.transitionTo('play',play_id + 1);
       }, function() {
         alert('failed to save play!');
