44a94cc5ac7a1db4d507068fdad3eaaa94f939ef
Rick Heller <rick.heller@yahoo.com>
2015-01-09 19:57:37 -0500

    its working with multiple pages now

---
+++ app/assets/javascripts/controllers/play_controller.js
@@ -0,0 +1,19 @@
+Ling.PlayController = Ember.ObjectController.extend({
+  actions: {
+    updatePlay: function(chosen, play_id) {
+
+     var play = this.store.createRecord('play', {
+        play_id: play_id,
+        success_rate: chosen
+      });
+
+      var controller = this;
+      play.save().then(function() {
+        console.log('play saved!');
+        controller.transitionTo('play',play_id + 1);
+      }, function() {
+        alert('failed to save play!');
+      });
+    }
+  }
+});
