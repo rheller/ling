29bbab97d227a3497fec592a051e1200e825b9f3
Rick Heller <rick.heller@yahoo.com>
2015-01-11 16:58:53 -0500

    reverse order of checking for showEmber and signedIn

--- app/assets/javascripts/routes/application_route.js
+++ app/assets/javascripts/routes/application_route.js
@@ -2,16 +2,18 @@ Ling.ApplicationRoute = Ember.Route.extend({
   beforeModel: function() {
     this.transitionTo('plays');
     var route = this;
-    //the purpose of this is to ping the server to determine whether
-    //the user is signed in or not
-    Ember.$.post('/users/sign_in', function() {
-      route.controllerFor('application').set('signedIn', true);
-    }).fail(function() {
-      route.controllerFor('application').set('signedIn', false);
-    });
+
     //only show ember on certain pages
     if (  $('#show-ember').length > 0) {
       route.controllerFor('application').set('showEmber', true);
+
+      //the purpose of this is to ping the server to determine whether
+      //the user is signed in or not
+      Ember.$.post('/users/sign_in', function() {
+        route.controllerFor('application').set('signedIn', true);
+      }).fail(function() {
+        route.controllerFor('application').set('signedIn', false);
+      });
     }
     else {
       route.controllerFor('application').set('showEmber', false);
