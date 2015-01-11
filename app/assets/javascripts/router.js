c948e13ae77867635882b89cd2f71da7df94b71d
Rick Heller <rick.heller@yahoo.com>
2015-01-11 17:19:51 -0500

    remove plays from router too

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,7 +1,6 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 Ling.Router.map(function() {
-  this.resource('plays');
   this.resource('play', {path: 'play/:play_id'});
   this.route('sign_in');
   this.route('sign_out');
