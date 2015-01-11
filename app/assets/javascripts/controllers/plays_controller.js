318705f8d3fdbb3cb4adf6b0e6b1c93397b769ba
Rick Heller <rick.heller@yahoo.com>
2015-01-11 17:15:14 -0500

    remove plays controller and template from ember

--- app/assets/javascripts/controllers/plays_controller.js
+++
@@ -1,8 +0,0 @@
-Ling.PlaysController = Ember.ArrayController.extend({
-  actions: {
-    updateLang: function() {
-      var controller = this;
-      controller.transitionToRoute('play', 1);
-    }
-  }
-});
