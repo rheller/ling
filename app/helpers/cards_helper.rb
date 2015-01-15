8c6e29fc967b8976e6c88cad6f12f452c4f732f1
Rick Heller <rick.heller@yahoo.com>
2015-01-15 10:13:05 -0500

    add screenshot image to front page

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,7 +1,7 @@
 module CardsHelper
   def success_rate(histories)
     if histories.empty?
-      "New!"
+      "New Word!"
     else
       "Answered correctly " + histories[0].successes.to_s + " out of " +  pluralize(histories[0].tries, 'try')
     end  
